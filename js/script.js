09d2c3ec5c0c3442cb940eb610500099a17166e7
Witzevds <witzvand@student.arteveldehs.be>
2024-04-21 12:46:53 +0200

    example flower randomized

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
-document.getElementById("applyColorBtn").addEventListener("click", function() {
-    // Haal de kleuren op uit de invoervelden
-    var roseColor = document.getElementById("roseColorInput").value;
-    var potColor = document.getElementById("potColorInput").value;
-    // Wijzig de vulkleur van de roos SVG
-    document.querySelector(".svg__rose path").style.fill = roseColor;
-    // Wijzig de vulkleur van de grond SVG
-    document.querySelector(".svg__soil path").style.fill = potColor;
+document.addEventListener("DOMContentLoaded", function () {
+  //dit spreekt de 2 de roos aan
+  const $roseSVG = document.getElementsByClassName("rose-svg")[1];
+  const $roseColorInput = document.getElementById("roseColorInput");
+  const $next = document.getElementById("next");
+
+  $next.addEventListener("click", function () {
+    console.log("done");
+
+    if ($roseSVG) {
+      $roseSVG.classList.toggle("green"); // Voeg de klasse "green" toe aan het geselecteerde SVG-element
+    } else {
+      console.error("Element with class 'rose-svg' not found.");
+    }
+    randomFlower();
+  });
 });
+
+const $exampleRoseSVG = document.getElementsByClassName("rose-svg")[0];
+let randomFlower = function () {
+  let flowers = ["blue", "green", "purple", "red", "yellow"];
+  randomIndex = Math.floor(Math.random() * flowers.length);
+  $exampleRoseSVG.classList.remove("blue", "green", "purple", "red", "yellow");
+  $exampleRoseSVG.classList.toggle(flowers[randomIndex]);
+  return flowers[randomIndex];
+};
